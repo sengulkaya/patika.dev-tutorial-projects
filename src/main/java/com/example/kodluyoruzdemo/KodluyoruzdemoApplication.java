6994a8eaa6291c716099cb42c800f089506fe870
Şengül Kaya <sengulkaya15@yahoo.com>
2022-01-28 18:26:53 +0300

    initial commit

---
+++ src/main/java/com/example/kodluyoruzdemo/KodluyoruzdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodluyoruzdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodluyoruzdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kodluyoruzdemo/KodluyoruzdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodluyoruzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
